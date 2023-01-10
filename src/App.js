5684f0c593be248859ab297eb6be39cbc154e614
FredRome <113112832+FredRome@users.noreply.github.com>
2023-01-10 12:57:27 +0100

    adding router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
 
-      </header>
-    </div>
+          <Route path="/core">
+            <CoreProduct />
+          </Route>
+          <Route path="/">
+            <Product />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
-export default App;
+function Product()) {
+  return <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSrDytNtQWGG-okdAqzLABY1_UOQQ1aHvwy8G-kCcHclMzafKqoGy3BnS401e_2lFEtceLmtuaXV1fF/embed?start=true&loop=true&delayms=10000" frameborder="0" width="1920" height="1080" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>;
+}
+
+function CoreProduct() {
+  return <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTw_BAxaSGp5NnMms-ga91yCDp6bL75RNac1ImAn8fGXSWuIgP6pOZ8qyOnCmcWt8gqzlJxN0JozpN0/embed?start=true&loop=true&delayms=10000" frameborder="0" width="1868" height="1080" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>;
+}
