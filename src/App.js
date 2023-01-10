b411c3f1aa7ed6deb5499ce80fdee45ed900f891
FredRome <113112832+FredRome@users.noreply.github.com>
2023-01-10 13:09:06 +0100

    bug

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default function App() {
   );
 }
 
-function Product()) {
+function Product() {
   return <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSrDytNtQWGG-okdAqzLABY1_UOQQ1aHvwy8G-kCcHclMzafKqoGy3BnS401e_2lFEtceLmtuaXV1fF/embed?start=true&loop=true&delayms=10000" frameborder="0" width="1920" height="1080" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>;
 }
 
