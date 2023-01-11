6f636c20e5f6eefb27c3b6289bc7776d30fe97ce
FredRome <113112832+FredRome@users.noreply.github.com>
2023-01-11 14:58:17 +0100

    adding a clock

--- src/routes/Prod.js
+++ src/routes/Prod.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import Clock from "../components/Clock";
 
 export default function Prod() {
   return (
     <div>
-
+    <div class="overlay">
+ <Clock />
+ </div>
 <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSrDytNtQWGG-okdAqzLABY1_UOQQ1aHvwy8G-kCcHclMzafKqoGy3BnS401e_2lFEtceLmtuaXV1fF/embed?start=true&loop=true&delayms=10000" frameborder="0" width="1920" height="1080" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+
     </div>
 
 
