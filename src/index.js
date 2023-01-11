668176b742b96730d91b1d93b8df898bdf5b4766
FredRome <113112832+FredRome@users.noreply.github.com>
2023-01-11 14:06:08 +0100

    adding HR slides

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Core from './routes/Core';
 import Prod from './routes/Prod';
+import Hr from './routes/Hr';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -14,6 +15,7 @@ ReactDOM.render(
         <Route path="/" element={<Prod />}  />
          <Route path="core" element={<Core />} />
          <Route path ="prod" element={<Prod />} />
+          <Route path ="hr" element={<Hr />} />
           <Route path="*" element={<h1>Route does notexist</h1>}/>
       </Routes>
   </BrowserRouter>
---
+++ src/routes/Hr.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Hr() {
+  return (
+    <div>
+<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQBUp-6ZRpJ4ipnvjOq13G_xYFAWXOxdXoe6N01XnPZP016pr8G1Dbkqr0c8yaLzfRI43DBciE-Su3y/embed?start=true&loop=true&delayms=10000" frameborder="0" width="1868" height="1080" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+    </div>
+
+
+  )
+}
