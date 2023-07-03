770f4b892840dbc2b7a6fc0cdfab8354feba8677
FredRome <113112832+FredRome@users.noreply.github.com>
2023-07-03 17:08:10 +0200

    adding Raincheck

---
+++ src/components/Rain.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+function Rain(){
+  return (
+    <span>
+
+    <iframe src="https://www.rainviewer.com/map.html?loc=55.6050,13.0038,9&oFa=0&oC=0&oU=0&oCS=1&oF=0&oAP=0&c=1&o=88&lm=0&layer=radar&sm=1&sn=1&hu=0" width="100%" frameborder="0" ></iframe>
+
+    </span>
+
+  );
+}
+export default Rain;
--- src/routes/Prod.js
+++ src/routes/Prod.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Clock from "../components/Clock";
-
+import Rain from "../components/Rain";
 export default function Prod() {
   return (
     <div>
-    <div class="overlay">
+    <div class="overlay-l">
  <Clock />
  </div>
+ <div class="overlay-r">
+<Rain />
+</div>
 <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSrDytNtQWGG-okdAqzLABY1_UOQQ1aHvwy8G-kCcHclMzafKqoGy3BnS401e_2lFEtceLmtuaXV1fF/embed?start=true&loop=true&delayms=10000" frameborder="0" width="1920" height="1080" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
 
     </div>
