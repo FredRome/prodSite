c37f69308032fec35a5838ad66a7006b6b9d3ff9
FredRome <113112832+FredRome@users.noreply.github.com>
2023-10-20 09:30:30 +0200

    Update Rain.js

--- src/components/Rain.js
+++ src/components/Rain.js
@@ -3,7 +3,7 @@ function Rain(){
   return (
     <span>
 
-    <iframe src="https://www.rainviewer.com/map.html?loc=55.6050,13.0038,9&oFa=0&oC=0&oU=0&oCS=1&oF=0&oAP=0&c=1&o=88&lm=0&layer=radar&sm=1&sn=1&hu=0" width="100%" height="100%" frameborder="0" ></iframe>
+    <iframe src="https://www.rainviewer.com/map.html?loc=55.635,12.9038,11&oFa=1&oC=1&oU=0&oCS=1&oF=0&oAP=0&c=7&o=83&lm=1&layer=radar&sm=1&sn=1&hu=false" width="100%" height="100%" frameborder="0" ></iframe>
 
     </span>
 
