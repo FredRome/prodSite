dcc4e88ce9b07cdcc6a3f9b69f1f757247cb9d59
FredRome <113112832+FredRome@users.noreply.github.com>
2024-02-29 09:20:00 +0100

    Update Prod.js

--- src/routes/Prod.js
+++ src/routes/Prod.js
@@ -12,9 +12,9 @@ export default function Prod() {
 <Rain />
 </div>
 <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSrDytNtQWGG-okdAqzLABY1_UOQQ1aHvwy8G-kCcHclMzafKqoGy3BnS401e_2lFEtceLmtuaXV1fF/embed?start=true&loop=true&delayms=10000" frameborder="0" width="1920" height="1080" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
-
-    </div>
     <JokeFooter />
+    </div>
+
 
 
   )
